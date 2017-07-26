bb20e035db925b8304187b4c498787f6335cf398
Vincent Zhou <zhouhdl@cn.ibm.com>
2017-07-26 10:05:44 +0800

    Nodejs initialization program

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hey Man', place:"Dalian Studio",message: 'This is a Devops Demo' });
+});
+
+module.exports = router;
