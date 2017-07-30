3b319ab17ef9bcb80d901ea93d76a49a975567fe
Vincent Zhou <zhouhdl@cn.ibm.com>
2017-07-30 12:19:38 +0800

    Feature:Update home page UI

--- spec/test.js
+++ spec/test.js
@@ -7,7 +7,6 @@ describe('Array', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
             assert.equal(-1, [1,2,3].indexOf(0));
-            assert.equal(-1, [1,2,3].indexOf(1));
         });
     });
 });
